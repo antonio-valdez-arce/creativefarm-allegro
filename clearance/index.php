f5fa96a62eca6d9fec7299e20eebde919a32cc13
Antonio Valdez Arce <a.valdez.arce@gmail.com>
2014-12-07 21:25:53 +0100

    Added images and html.

---
+++ clearance/index.php
@@ -0,0 +1,127 @@
+<?php
+    $host = 'http://localhost/creativefarm-allegro/clearance';
+    $images_path = $host . '/img';
+    
+    $width = 960;
+    $table_width = '100%';
+    $uid = 0000;
+    $url_complaints = '#';
+?>
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+
+<body>
+
+<div style="border: #b4b4b4 solid; border-width: 0 1px 1px; color: #000; display: block; font-family: Arial, sans; font-size: 14px; margin: 0 auto 40px auto; padding: 0 0 40px 0; width: <?php echo $width?>px;">
+	<table border="0" cellpadding="0" style="background: transparent url(<?php echo $images_path ?>/separator.jpg) no-repeat left bottom; font-size: 14px; padding: 0 0 20px 0; width:<?php echo $table_width; ?>;">
+		<tbody>
+			<tr>
+				<td style="text-align: right; padding: 0 20px 0 0;"><b>tel. 782 152 383</b></td>
+			</tr>
+			<tr>
+				<td style="text-align: right; padding: 0 20px 0 0;"><b>info@creativefarm.com.pl</b></td>
+			</tr>
+			<tr>
+				<td>
+					<a href="creativefarm.com.pl" style="color: #000; text-decoration: none">
+						<img src="<?php echo $images_path ?>/header_logo.jpg" alt="Creative Farm" />
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td style="text-align: center;">
+					<a href="" style="color: #000; text-decoration: none;">o nas</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; 
+					<a href="" style="color: #000; text-decoration: none;">inne aukcje</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; 
+					<a href="" style="color: #000; text-decoration: none;">komentarze</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; 
+					<a href="" style="color: #000; text-decoration: none;">dodaj do ulubionych</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+	<table border="0" cellpadding="0" style="background: transparent url(<?php echo $images_path ?>/separator.jpg) no-repeat left bottom; font-size: 12px; padding: 0 0 20px 0; width:<?php echo $table_width; ?>;">
+		<tbody>
+			<tr>
+				<td width="520" style="padding: 25px 0 0 35px">
+					<img src="<?php echo $images_path ?>/butterfly_1.jpg" alt="Creative Farm" />
+					<br /><br />
+					<img src="<?php echo $images_path ?>/butterfly_2.jpg" alt="Creative Farm" />
+				</td>
+				<td width="440" valign="top">
+					<h2 style="font-size: 24px;">Motyl Papylo Ulysses w gablocie</h2>
+					<h3 style="font-size: 16px;">Cena motyla w gablocie: 150 zł</h3>
+					
+					<p>Wymiary gabloty: 32×26 cm<br/>
+					Rozpiętość skrzydeł: ok. 14 cm<br/>
+					Występowanie: Australazja (Australia, Nowa Zelandia, Nowa Gwinea)<br/>
+					Kolor: Brzegi skrzydeł czarne, w środku opalizujący niebieski. <br/>
+					Na tylnych skrzydłach posiada dwa promieniste ogony.</p>
+					
+					<p>Duży motyl w kolorze błękitnej laguny. Ręcznie robiona ramka<br/>
+					tworzy z nim spójną całość i przywodzi na myśl dzieła sztuki.</p>
+					
+					<p><b style="color: #cf1111;">UWAGA!</b><br/>
+					W zamówieniu prosimy o wpisanie kolory ramy i passpartout.</p>
+					<br /><br /><br /><br /><br />
+					<img src="<?php echo $images_path ?>/available_colors.jpg" alt="Creative Farm" />
+				</td>
+			</tr>
+			
+		</tbody>
+	</table>
+	
+	<table border="0" cellpadding="0" style="background: transparent url(<?php echo $images_path ?>/separator.jpg) no-repeat left bottom; font-size: 12px; padding: 25px 0 20px 35px; width:<?php echo $table_width; ?>;">
+		<tbody>
+			<tr>
+				<td width="380"><img src="<?php echo $images_path ?>/payment_delivery.jpg" alt="płatności i dostawa" /></td>
+				<td width="280" style="padding: 0 0 0 20px;"><img src="<?php echo $images_path ?>/contact.jpg" alt="kontakt" /></td>
+				<td style="padding: 0 0 0 20px;"><img src="<?php echo $images_path ?>/complaints.jpg" alt="Reklamacje i zwroty" /></td>
+			</tr>
+			<tr>
+				<td valign="top">
+					<p><b>Poczta Polska:</b><br/>9,50 zł – paczka nieprzekraczająca ciężaru 1kg</p>
+					<p><b>Kurier:</b><br/>16,50 – paczka krajowa do 5 kg za pośrednictwem K-EX<br/>
+					18,50 – paczka krajowa do 5 kg za pośrednictwem UPS</p>
+
+					<p>- - - - - - - - - - - - - - - - - - - - - - - - -</p> 
+					
+					<p>Przesyłki wysyłamy następnego dnia po otrzymaniu<br/>
+					zamówienia. Czas oczekiwania na przesyłkę<br/>
+					– do 3 dni roboczych.</p>
+				</td>
+				<td valign="top" style="line-height: 1.3; padding: 0 0 0 20px;">
+					<p>Jagoda Teodorowicz:  782 152 383<br/>
+					Tomasz Teodorowicz:  502 269 625<br/><br/>
+					Prosimy dzwonić od poniedziałku<br/>
+					do piątku w godzinach od 9:00 – 17:00<br/><br/>
+					e-mail: info@creativefarm.com.pl</p>
+				</td>
+				<td valign="top" style="padding: 0 0 0 20px;">
+					<p>Informacje na temat składania 
+					reklamacji znajdują się <a href="<?php echo $url_complaints; ?>" style="color: #2b19b3">tutaj</a>.</p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	
+	<table border="0" cellpadding="0" style="background: transparent url(<?php echo $images_path ?>/separator.jpg) no-repeat left bottom; font-size: 12px; padding: 25px 0 20px 35px; width:<?php echo $table_width; ?>;">
+		<tbody>
+			<tr>
+				<td>
+					<img src="<?php echo $images_path ?>/who_is_creativefarm.jpg" alt="Creative Farm" />	
+				</td>
+				<td style="background: #3a432c; color: #fff; padding: 35px 35px 35px 50px">
+					<p>Jesteśmy niewielką, rodzinną firmą. Łączymy kobiecy gust i wyczucie piękna oraz męska dokładność<br/> 
+					i konstruktorskie wykształcenie. Unikatowe pomysły i realizacje prezentujemy na naszej stronie.</p>
+					<p>Tworzymy rzeczy niebanalne, łączące w sobie style z pogranicza wiejskiej rezydencji i nowoczesnego designu.<br/> 
+					Pragniemy stać się inspiracją dla wyjątkowych osób i wspólnie z Państwem odmieniać domową przestrzeń.<br/>  
+					Dokładamy starań aby nasze produkty zaspokoiły najbardziej wymagających klientów a marka Crative Farm była<br/>  
+					synonimem unikalności i kreatywności.</p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+
+</body>
+</html>
